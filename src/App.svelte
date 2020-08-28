f0b6fbf8259d0e2cec1bdcf13c635a70021d9982
Cody Averett <codyaverett@gmail.com>
2020-08-28 02:58:28 -0500

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
   export let date;
 
   onMount(async () => {
-    const res = await fetch("/api/date");
+    const res = await fetch("/.netlify/api/date");
     const newDate = await res.text();
     date = new Date();
   });
